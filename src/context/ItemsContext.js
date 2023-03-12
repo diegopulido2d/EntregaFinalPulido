0c8f0ae3712b8a8f8b843e6a1ca709c8ea80356b
Diego Felipe Pulido <dpulido@titamedia.com>
2023-03-12 17:59:29 -0500

    Ajustes

--- src/context/ItemsContext.js
+++ src/context/ItemsContext.js
@@ -4,7 +4,7 @@ export const ItemsContext = createContext();
 
 export const ItemsProvider = ({ children }) => {
   const [items, setItems] = useState([]);
-  const[cartQuantity, setCartQuantity] = useState(0);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   return (
     <ItemsContext.Provider value={{items, setItems, cartQuantity, setCartQuantity}}>
